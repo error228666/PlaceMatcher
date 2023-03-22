0033d1b7efefa6f1f5e354f01462d395e63e93f2
maria <mary1voight@gmail.com>
2023-03-22 17:21:59 +0300

    Setting up elastic search. updated requirements

--- src/placematcher/settings.py
+++ src/placematcher/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_elasticsearch_dsl',
     "mainpage",
     "core",
     "search",
+    "elastic_search",
 ]
+
+ELASTICSEARCH_DSL = {
+    'default': {
+        'hosts': 'localhost:9200'
+    },
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
